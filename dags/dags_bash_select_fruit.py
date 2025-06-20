797ad61a10160a022b81c03714efe762445068cf
moon c <moonb_c>
2025-06-20 18:01:58 +0900

    select_fruit.sh 수행하는 dag 업로드

---
+++ dags/dags_bash_select_fruit.py
@@ -0,0 +1,21 @@
+import pendulum
+from airflow.sdk import DAG, chain
+from airflow.providers.standard.operators.bash import BashOperator
+
+with DAG(
+    dag_id="dags_bash_select_fruit",
+    schedule="10 0 * * 6#1",
+    start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
+    catchup=False
+) as dag:
+    bash_t1_orange = BashOperator(
+        task_id="bash_t1_orange",
+        bash_command="opt/airflow_t/plugins/shell/selet_fruit.sh ORANGE",
+    )
+
+    bash_t2_avocado = BashOperator(
+        task_id="bash_t2_avocado",
+        bash_command="opt/airflow_t/plugins/shell/selet_fruit.sh AVOCADO",
+    )
+
+    bash_t1_orange >> bash_t2_avocado
